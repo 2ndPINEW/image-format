6dc35482c5590da4f3a4da9c67901da597615e40
Eito Takase <eito01178@gmail.com>
2023-04-27 18:00:29 +0900

    ファイルサイズとかも比較する

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const sharp = require("sharp");
+import { readFileSync } from "fs";
 
 interface ConvertReport {
   data: any;
@@ -7,30 +8,62 @@ interface ConvertReport {
 
 const assetBase = "src/assets/";
 
-const convertToWebP = async (fileName: string): Promise<ConvertReport> => {
-  return await sharp(`${assetBase}${fileName}`).webp().toBuffer();
+const convertToWebP = async (path: string): Promise<ConvertReport> => {
+  return await sharp(path).webp().toBuffer();
 };
 
-const convertToAVIF = async (fileName: string): Promise<ConvertReport> => {
-  return await sharp(`${assetBase}${fileName}`).avif().toBuffer();
+const convertToAVIF = async (path: string): Promise<ConvertReport> => {
+  return await sharp(path).avif().toBuffer();
 };
 
-const measureTime = async (func: Function) => {
-  const fileNames = new Array(5).fill("").map(() => `0.png`);
+const measureTime = async (paths: string[], func: Function) => {
+  const fileSizes: number[] = [];
 
   const start = Date.now();
-  for await (const fileName of fileNames) {
-    await func(fileName);
+  for await (const path of paths) {
+    const buffer = await func(path);
+    fileSizes.push(buffer.length);
   }
   const end = Date.now();
-  return end - start;
+
+  const reports = paths.map((path, index) => {
+    const compressedFileSize = fileSizes[index];
+    const originalFileSize = readFileSync(path).length;
+    const compressPercentage = (compressedFileSize / originalFileSize) * 100;
+    return {
+      compressPercentage,
+      compressedFileSize,
+      originalFileSize,
+    };
+  });
+
+  const averageCompressPercentage =
+    reports.reduce((acc, cur) => acc + cur.compressPercentage, 0) /
+    reports.length;
+
+  return {
+    progressMs: end - start,
+    reports,
+    averageCompressPercentage,
+  };
 };
 
 const main = async () => {
-  const webPTime = await measureTime(convertToWebP);
-  const avifTime = await measureTime(convertToAVIF);
-  console.log(`WebP: ${webPTime}ms`);
-  console.log(`AVIF: ${avifTime}ms`);
+  const paths = new Array(10).fill("").map(() => `${assetBase}0.png`);
+  console.log(
+    `${paths.length}枚の画像を変換して、Bufferに書き出すのにかかった時間を計測します\n`
+  );
+
+  const webpResult = await measureTime(paths, convertToWebP);
+  console.log("WebP --------------------");
+  console.log(`ProgressTime: ${webpResult.progressMs / 1000}s`);
+  console.log(`FileSize: ${webpResult.averageCompressPercentage}%`);
+
+  const avifResult = await measureTime(paths, convertToAVIF);
+
+  console.log("AVIF --------------------");
+  console.log(`ProgressTime: ${avifResult.progressMs / 1000}s`);
+  console.log(`FileSize: ${avifResult.averageCompressPercentage}%`);
 };
 
 main();
